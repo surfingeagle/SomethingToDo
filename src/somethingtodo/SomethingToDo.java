5cc6d9bd1667d5f4f60c01bd23e31c873480c6bb
surfingeagle <keithnoll@outlook.com>
2017-03-05 13:06:31 -0500

    Test commit

---
+++ src/somethingtodo/SomethingToDo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package somethingtodo;
+import my.aJFrame.aJFrame;
+
+/**
+ *
+ * @author Keith
+ */
+public class SomethingToDo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new aJFrame().setVisible(true);
+            }
+        });
+    }
+    
+}
